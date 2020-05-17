2c1138a36d1507e4b7858e2e68412f780cd6be3d
Péter Vida <petike997@gmail.com>
2020-05-17 09:24:32 +0200

    delete-publish start

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -5,28 +5,48 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+
 })
 export class HomeComponent {
   public areas: string[]
   public retroItems: RetroItem[];
 
+  public retroTable: RetroItem[];
+
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<string[]>(baseUrl + 'api/retro/areas').subscribe(result => {
       this.areas = result;
 
-      let item1 = { text: "item1", area: this.areas[0] };
-      let item2 = { text: "item2", area: this.areas[1] };
-      let item3 = { text: "item3", area: this.areas[2] };
+      let item1 = {id: 1, text: "item1", area: this.areas[0] };
+      let item2 = {id: 2, text: "item2", area: this.areas[1] };
+      let item3 = {id: 3, text: "item3", area: this.areas[2] };
       this.retroItems = [item1, item2, item3];
     });
   };
 
-  addItem(item : RetroItem) {
+  addItem(newitemForm: NgForm) {
+    let item = newitemForm.value;
+    let currentItemCount = this.retroItems.length;
+    item.id = Math.max(this.retroItems.length + 1, this.retroItems.slice(-1)[0].id + 1);
     this.retroItems.push(item);
+    newitemForm.reset();
+  }
+
+  removeItem(id: number) {
+    this.retroItems = this.retroItems.filter(item => item.id != id);
+  }
+
+  publishItem(id: number) {
+    let publishedItem = this.retroItems.filter(item => item.id == id)[0];
+    publishedItem.id = undefined;
+    this.retroTable.push(publishedItem);
+    this.removeItem(id);
+    // TODO: notify server about the published item
   }
 }
 
 export class RetroItem {
+  id?: number
   text: string;
   area: string;
 }
